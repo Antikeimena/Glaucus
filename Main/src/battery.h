f5ee3de8e857deddb82b7b6d3fd3348bc388b5b1
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2018-05-26 23:18:03 +0200

    Off by factor 10 :-)

--- Main/src/battery.h
+++ Main/src/battery.h
@@ -2,7 +2,7 @@
 #define BATTERY_H
 
 #define BATTERY_MEASURE_NUM                 100.0
-#define BATTERY_VOLTAGE_FACTOR     ( 1.0 /  232.0)
+#define BATTERY_VOLTAGE_FACTOR     (10.0 /  232.0)
 #define BATTERY_CONTROL_INTERVAL   (10   * 1000)
 
 extern float battery_voltage;
